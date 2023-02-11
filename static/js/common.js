c87dd19dc6da56adc2275fd8208e7ed2a99c70ff
fear220268 <devsequencce@gmail.com>
2023-02-11 22:40:51 +0200

    fix site

--- static/js/common.js
+++ static/js/common.js
@@ -1,13 +1,24 @@
 $('.header-btn').on('click', function (e) {
     var $this = $(this);
     $this.toggleClass('active');
-    $('body').toggleClass('scroll');
     $('.header').toggleClass('active');
+    $('body').toggleClass('scroll');
 });
-$('.header-bottom li').on('click', function (e) {
-    var $this = $(this);
-    $this.toggleClass('active').find('ul').slideToggle();
-});
+if ($(window).width() < 992) {
+    $('.header-bottom li').on('click', function (e) {
+        var $this = $(this);
+        $this.toggleClass('active').find('.sub-nav').slideToggle();
+    });
+}else{
+    $('.has-sub').on( "mouseenter",  function() {
+        var $this = $(this);
+        $('.sub-nav').removeClass('active');
+        $this.find('.sub-nav').addClass('active');
+    });
+    $('.header-bottom').on( "mouseleave", function() {
+        $('.sub-nav').removeClass('active')
+    });
+}
 
 $(".team-slider").slick({
     speed: 500,
@@ -81,7 +92,7 @@ $('.numbers-item__number span').each(function (e) {
 $('.price-table__btn').on('click', function (e) {
     e.preventDefault();
     var $this = $(this);
-    $this.find('span').toggleClass('active');
+    $this.find('span').toggleClass('hidden');
     $this.prev().toggleClass('active');
 });
 $('.faq-item__title').on('click', function (e) {
@@ -91,3 +102,26 @@ $('.faq-item__title').on('click', function (e) {
     return false;
 });
 
+function popupOpen() {
+    var $popupButton = $('.btn-popup');
+    $popupButton.on('click', function (e) {
+        var $this = $(this);
+        var popupButtonData = $this.data('popup');
+        $('.popup').removeClass('active');
+        $('div[data-popup = '+popupButtonData+']').addClass('active');
+        // $('body').addClass('scroll');
+    });
+}
+popupOpen();
+$('.popup-close').on('click', function (e) {
+    var $this = $(this);
+    $this.parent().parent().removeClass('active');
+    $('.popup-overlay').removeClass('active');
+    // $('body').removeClass('scroll');
+});
+$('.popup-overlay').on('click', function (e) {
+    var $this = $(this);
+    $this.removeClass('active');
+    $('.popup').removeClass('active');
+    // $('body').removeClass('scroll');
+});
